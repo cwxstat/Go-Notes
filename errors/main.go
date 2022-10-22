0878de8dac78664c7ce140bf0549635b8586425a
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2022-10-22 18:09:08 -0400

    Adding custom error messages

---
+++ errors/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+// FooError records an error and what caused it
+type FooError struct {
+	Op       string
+	FooThing string
+	Err      error
+}
+
+func ErrorCreator() *FooError {
+
+	return &FooError{
+		Op:       "was eating miso soup",
+		FooThing: "just happend",
+		Err:      errors.New("bad fortune cookie"),
+	}
+
+}
+
+func (e *FooError) Error() string { return e.Op + " " + e.FooThing + ": " + e.Err.Error() }
+
+func (e *FooError) Unwrap() error { return e.Err }
+
+func (e *FooError) Thing() string { return e.FooThing }
+
+func main() {
+	if err := ErrorCreator(); err != nil {
+		var fooError *FooError
+		if errors.As(err, &fooError) {
+			fmt.Println("Explain failure thing...:", fooError.Thing())
+		} else {
+			fmt.Println(err)
+		}
+	}
+
+}
